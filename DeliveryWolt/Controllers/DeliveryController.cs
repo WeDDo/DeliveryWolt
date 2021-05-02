d0dcc4268c4d8c9a10609c5fc18bdf1e545da08a
WeDDo <mantuxas001@gmail.com>
2021-05-02 19:06:44 +0300

    little update

---
+++ DeliveryWolt/Controllers/DeliveryController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DeliveryWolt.Models;
+
+namespace DeliveryWolt.Controllers
+{
+    public class DeliveryController : Controller
+    {
+        List<Delivery> deliveries = new List<Delivery>() {
+                new Delivery(0, 10, 10, true, new List<Package>{ new Package(0, "11x11x11", 10, new DateTime(2011,11,11), "AA", "AAA", 12.5, false, 7 )})
+            };
+
+        // GET: Delivery
+        public ActionResult DeliveryManPage()
+        {
+            return View();
+        }
+
+        [ActionName("DeliveryView")]
+        public ActionResult openDeliveryView()
+        {
+            Delivery delivery = getDeliveries()[0];
+
+            return displayDeliveryPage(delivery);
+        }
+
+        public List<Delivery> getDeliveries()
+        {
+            return deliveries;
+        }
+
+        public ActionResult displayDeliveryPage(Delivery delivery)
+        {
+            return View("DeliveryView", delivery);
+        }
+    }
+}
---
+++ DeliveryWolt/Models/Delivery.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DeliveryWolt.Models
+{
+    public class Delivery
+    {
+        public int Id { get; set; }
+        public double Cost { get; set; }
+        public double TotalDistance { get; set; }
+        public bool Displayed { get; set; }
+
+        public List<Package> packages;
+
+        public Delivery(int id, double cost, double totalDistance, bool displayed, List<Package> packages)
+        {
+            Id = id;
+            Cost = cost;
+            TotalDistance = totalDistance;
+            Displayed = displayed;
+            this.packages = packages;
+        }
+    }
+}
